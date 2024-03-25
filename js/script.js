76320ed5d286c557245c860e8bc2985366f23557
telecasteren <127958662+telecasteren@users.noreply.github.com>
2024-03-25 17:16:18 +0100

    changes after feedback

--- js/script.js
+++ js/script.js
@@ -1,62 +1,34 @@
-console.log("We have connection");
-
-// Fetching the HTML element and making a page heading
+// Fetching the HTML elements
 const heading = document.querySelector(".title");
+const timer = document.querySelector(".timer");
+let button = document.querySelector("button");
 
 heading.innerHTML = "<h1>" + "What time is it?!" + "</h1>";
 
-// Building the real time clock counting up towards 24 hours (0-23)
-
-const timer = document.querySelector(".timer");
-
+// Building the real time clock with setTimeout() and setInterval()
 function realTimeClock() {
   let currentTime = new Date().getTime();
-  let timeDifference = currentTime;
 
-  const seconds = Math.floor((timeDifference / 1000) % 60);
-  const minutes = Math.floor((timeDifference / (1000 * 60)) % 60);
-  const hours = Math.floor((timeDifference / (1000 * 60 * 60)) % 24);
+  let seconds = Math.floor((currentTime / 1000) % 60);
+  let minutes = Math.floor((currentTime / (1000 * 60)) % 60);
+  let hours = Math.floor((currentTime / (1000 * 60 * 60)) % 24);
 
   document.querySelector("#seconds").innerHTML = seconds;
   document.querySelector("#minutes").innerHTML = minutes;
   document.querySelector("#hours").innerHTML = hours + 1;
   //not sure how to get correct timeZone, so added +1 for now
-}
 
+  setTimeout(realTimeClock, 1000);
+}
 realTimeClock();
 
-setInterval(realTimeClock, 1000);
+//setInterval(realTimeClock, 1000);
 
-// TO DO
-
-// 1. Find a way to implement the setTimeout() method
-// 2. Update the design - make it look like a real clock
-// 3. Local time - EX: let s= new Date().toLocaleString();
-
-let button = document.querySelector("button");
-
-// Testing button function
 // A button click will pause the clock until alert message is gone
 button.onclick = function () {
-  //setTimeout(realTimeClock, 6000);
   console.log("Button clicked!");
   alert("Button clicked!");
 };
-
-// Button styles -------------------------
-button.style.backgroundColor = "var(--light-blue)";
-button.style.color = "black";
-button.style.borderColor = "var(--light-blue)";
-button.style.borderRadius = "12px";
-button.style.border = "1px";
-button.style.width = "200px";
-button.style.height = "50px";
-button.style.marginTop = "10%";
-button.style.fontWeight = "bolder";
-button.style.fontSize = "25px";
-button.style.justifySelf = "center";
-button.style.cursor = "pointer";
-//----------------------------------------
 
 // onmouseover function - hover effect with setTimeout method
 function hoverColor() {
